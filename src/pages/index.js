a39e3aaaa1f5b66f7cbcfb5b170db07b615e4a1a
martin-banks <anotherbanksy@gmail.com>
2021-01-23 20:02:28 +1100

    removes unused dashboard. adds webcam compoent

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,13 @@ import Styled from 'styled-components'
 
 // import Image from "../components/image"
 import SEO from "../components/seo"
-import Dashboard from '../components/dashboard'
+import WebCam from '../components/webcam'
 
 
 const IndexPage = () => (
   <>
     <SEO title="Home" />
-    <Dashboard />
+    <WebCam />
   </>
 )
 
