5d3a3931d5a4e708c8fc1d7c9f34d889f0facf23
martin-banks <anotherbanksy@gmail.com>
2021-01-24 15:34:00 +1100

    fixes right side image both halves flipped

--- src/components/webcam.js
+++ src/components/webcam.js
@@ -27,9 +27,8 @@ const Video = Styled.video`
 `
 
 function groupPixels (pixels) {
-  // if (!data) return null
-  // const pixels = [...data]
-  console.log({ pixels })
+  // if (!pixels) return null
+  // console.log({ pixels })
   const count = pixels?.data?.length / 4
   const output = {
     width: pixels.width,
@@ -39,41 +38,42 @@ function groupPixels (pixels) {
   for (let i = 0; i < count; i++) {
     const start = 4 * i
     const end = start + 4
-    // pixels.data[start + 1] = 255
     output.data.push([...pixels.data.slice(start, end)])
   }
-  // return output
-  // console.log({ output })
-  const mirrored = arrayMirror({ ratio, data: output.data }).left
-  // console.log({ mirrored })
-  // return
-  const mirrorSplit = []
-  for (let i = 0; i < mirrored.length; i++) {
-    mirrorSplit.push(...mirrored[i])
+
+  const mirrored = arrayMirror({ ratio, data: output.data })
+
+  const mirrorSplitLeft = []
+  const mirrorSplitRight = []
+  for (let i = 0; i < mirrored.left.length; i++) {
+    mirrorSplitLeft.push(...mirrored.left[i])
+    mirrorSplitRight.push(...mirrored.right[i])
   }
 
-  // const newSize = Math.sqrt(mirrorSplit.length / 4)
-  console.log({ mirrorSplit })
+  const readyToRender = {
+    left: new ImageData(
+      Uint8ClampedArray.from(mirrorSplitLeft),
+      pixels.width,
+      pixels.height,
+    ),
+    right: new ImageData(
+      Uint8ClampedArray.from(mirrorSplitRight),
+      pixels.width,
+      pixels.height,
+    ),
+  }
 
-  const readyToRender = new ImageData(
-    Uint8ClampedArray.from(mirrorSplit),
-    // newSize,
-    // newSize
-    pixels.width,
-    pixels.height,
-  )
-
-  console.log({ readyToRender })
   return readyToRender
 }
 
 const WebCam = () => {
   const videoRef = useRef(null)
   const canvasRef = useRef(null)
-  const previewRef = useRef(null)
+  const previewRefLeft = useRef(null)
+  const previewRefRight = useRef(null)
 
-  const [ pixelState, updatePixelState ] = useState(null)
-  const [ newPixels, setNewPixels ] = useState(null)
+  // const [ pixelState, updatePixelState ] = useState(null)
+  // const [ newPixels, setNewPixels ] = useState(null)
 
   useEffect (() => {
     navigator.mediaDevices.getUserMedia({
@@ -88,19 +88,19 @@ const WebCam = () => {
         videoRef.current.srcObject = localMediaStream
         videoRef.current.play()
         const ctx = canvasRef.current.getContext('2d')
-        const previewCtx = previewRef.current.getContext('2d')
+        const previewCtxLeft = previewRefLeft.current.getContext('2d')
+        const previewCtxRight = previewRefRight.current.getContext('2d')
         let updateLoop = null
 
         setTimeout(() => {
-          // console.log(videoRef.current.videoWidth)
-
           canvasRef.current.width = videoWidth
           canvasRef.current.height = videoWidth
 
-          previewRef.current.width = videoWidth
-          previewRef.current.height = videoWidth
+          previewRefLeft.current.width = videoWidth
+          previewRefLeft.current.height = videoWidth
 
-          // console.log({ pixelState })
+          previewRefRight.current.width = videoWidth
+          previewRefRight.current.height = videoWidth
 
           updateLoop = setInterval(() => {
             ctx.drawImage(
@@ -109,43 +109,28 @@ const WebCam = () => {
               videoRef.current.videoWidth,
               videoRef.current.videoHeight
             )
-
-            // setNewPixels()
-
-            // console.log(Uint8ClampedArray.from([...ctx.getImageData(0, 0, videoWidth, videoWidth).data]))
-
-
-            previewCtx.putImageData(
-              groupPixels(ctx.getImageData(0, 0, videoWidth, videoWidth)),
+            previewCtxLeft.putImageData(
+              groupPixels(ctx.getImageData(0, 0, videoWidth, videoWidth)).left,
               0,0,0,0,
               videoWidth,
               videoWidth,
             )
-
-            if (pixelState) {
-              console.log('new pixels!!', pixelState)
-              // previewCtx.drawImage(
-              //   '',
-              //   0, 0,
-              //   videoRef.current.videoWidth,
-              //   videoRef.current.videoHeight
-              // )
-
-              
-            } else {
-              return
-            }
-
+            previewCtxRight.putImageData(
+              groupPixels(ctx.getImageData(0, 0, videoWidth, videoWidth)).right,
+              0,0,0,0,
+              videoWidth,
+              videoWidth,
+            )
             // get the pixels of the image
             // canvas width and height is available on this pixels object
             // Useful for dynamic sizing
             // const pixels = ctx.getImageData(0, 0, videoWidth, videoWidth)
             // updatePixelState(pixels)
-
           }, 32) // interval update
         }, 1000)
 
         setTimeout(() => {
+          // Cancel the update loop after timep period
           clearInterval(updateLoop)
         }, 10 * 1000)
       })
@@ -155,9 +140,10 @@ const WebCam = () => {
   })
 
   return <div>
-    <Preview ref={ previewRef } />
+    <Preview ref={ previewRefLeft } />
     <Canvas ref={ canvasRef } />
     <Video ref={ videoRef } />
+    <Preview ref={ previewRefRight } />
     {/* <pre>
       pixels length: { pixelState?.length } | 
       grouped: { groupPixels(pixelState)?.length }
