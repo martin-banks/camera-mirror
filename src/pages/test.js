1ca29729d31f9b54f6eb17c8aa9dae871b7d71ab
martin-banks <anotherbanksy@gmail.com>
2021-01-29 18:55:53 +1100

    tests window size hook

--- src/pages/test.js
+++ src/pages/test.js
@@ -6,23 +6,34 @@ import Styled from 'styled-components'
 import SEO from "../components/seo"
 import Logo from '../components/logo'
 
-import timer from '../components/timer-hooks'
+// import timer from '../components/timer-hooks'
+import useWindowSize from '../components/window-resize-hook'
 
 
 
+const Box = Styled.div`
+  width: ${p => p.width}px;
+  height: ${p => p.height}px;
+  outline: solid 2px red;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+`
 
 
-  
 const TestPage = () => {
+  const [ width, height ] = useWindowSize()
   // console.log('this is the timer', { timer })
-  const {
-    isRunning,
-    // elapsedTime,
-    // setElapsedTime,
-    time,
-    setTime,
-    setIsRunning,
-  } = timer();
+  // const {
+  //   isRunning,
+  //   // elapsedTime,
+  //   // setElapsedTime,
+  //   time,
+  //   setTime,
+  //   setIsRunning,
+  // } = timer();
   
   // useEffect(() => {
   //   // startTimer()
@@ -32,21 +43,28 @@ const TestPage = () => {
 
   // }, [isRunning])
 
-  const handleClick = () => {
-    if (isRunning) {
-      setIsRunning(false)
-    } else {
-      // setElapsedTime(0)
-      setIsRunning(true)
-    }
-  }
+  // const handleClick = () => {
+  //   if (isRunning) {
+  //     setIsRunning(false)
+  //   } else {
+  //     // setElapsedTime(0)
+  //     setIsRunning(true)
+  //   }
+  // }
   return <>
-    <SEO title="Home" />
+    <SEO title="Test page" />
     <Logo />
-    <pre className="dump">{ Math.round(time) }</pre>
-    <button onClick={ handleClick }>{
+    {/* <pre className="dump">{ Math.round(time) }</pre> */}
+    {/* <button onClick={ handleClick }>{
       isRunning ? 'Stop' : 'Start'
-    }</button>
+    }</button> */}
+
+
+    <Box width={ width / 4 } height={ height / 4 } />
+
+
+
+    {/* <WindowSize /> */}
   </>
 }
 
