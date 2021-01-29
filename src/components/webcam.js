fe4787fab67eed7eeb6f2c0ba50a2332c308af55
martin-banks <anotherbanksy@gmail.com>
2021-01-29 15:16:22 +1100

    moves webcam functioality to separate file. adds countdown

--- src/components/webcam.js
+++ src/components/webcam.js
@@ -2,51 +2,16 @@ import React, { useEffect, useRef, useState } from 'react'
 import Styled from 'styled-components'
 import PropTypes from 'prop-types'
 
-import arrayMirror from '../functions/array-mirror'
+import captureFromWebcam from '../functions/capture-from-webcam'
 
 import MirrorPreview from './mirror-preview'
+import PlayIcon from './icon-play'
+import timer from './timer-hooks'
 
 
-const videoWidth = 400
-const ratio = [ 400, 400 ]
-
-
-function groupPixels (pixels) {
-  const count = pixels?.data?.length / 4
-  const output = {
-    width: pixels.width,
-    height: pixels.height,
-    data: [],
-  }
-  for (let i = 0; i < count; i++) {
-    const start = 4 * i
-    const end = start + 4
-    output.data.push([...pixels.data.slice(start, end)])
-  }
-
-  const mirrored = arrayMirror({ ratio, data: output.data })
-
-  const mirrorSplitLeft = []
-  const mirrorSplitRight = []
-  for (let i = 0; i < mirrored.left.length; i++) {
-    mirrorSplitLeft.push(...mirrored.left[i])
-    mirrorSplitRight.push(...mirrored.right[i])
-  }
-
-  const readyToRender = {
-    left: new ImageData(
-      Uint8ClampedArray.from(mirrorSplitLeft),
-      pixels.width,
-      pixels.height,
-    ),
-    right: new ImageData(
-      Uint8ClampedArray.from(mirrorSplitRight),
-      pixels.width,
-      pixels.height,
-    ),
-  }
-
-  return readyToRender
+const videoSize = {
+  width: 400,
+  height: 400,
 }
 
 
@@ -73,9 +38,37 @@ const CanvasStaging = Styled.canvas`
   height: 0;
 `
 const Video = Styled.video`
-  width: 400px; // ${p => videoWidth / 2}px;
-  height: 400px; // ${p => videoWidth / 2}px;
-  outline: solid 1px pink;
+  width: 400px; // ${p => videoSize.width / 2}px;
+  height: 400px; // ${p => videoSize.height / 2}px;
+  background: black;
+`
+const ControlsWrapper = Styled.section`
+  display: grid;
+  justify-content: center;
+  padding: 6rem 0;
+`
+const Button = Styled.button`
+  background: none;
+  color: none;
+  border: none;
+  outline: none;
+  box-shadow: none;
+  cursor: pointer;
+  &:hover {
+    svg path {
+      fill: gold !important;
+    }
+  }
+`
+
+const CountdownWrapper = Styled.div`
+  display: grid;
+  justify-content: center;
+`
+const Countdown = Styled.span`
+  font-size: 10rem;
+  text-align: center;
+  font-family: dharma-gothic-e, sans-serif;
 `
 
 
@@ -86,92 +79,69 @@ const WebCam = () => {
   const previewRefLeft = useRef(null)
   const previewRefRight = useRef(null)
 
+  const [ cameraLive, setCameraLive ] = useState(false)
+  const [ duration, setDuration ] = useState(5000)
+  const [ showTimer, setShowTimer ] = useState(false)
+
+  const {
+    time,
+    setTime,
+    isRunning,
+    setIsRunning,
+  } = timer()
+
   useEffect (() => {
-    return
-    navigator.mediaDevices.getUserMedia({
-      audio: false,
-      video: {
-        width: videoWidth,
-        height: videoWidth,
-      },
-    })
-      .then(localMediaStream => {
-        console.log(localMediaStream)
-        videoRef.current.srcObject = localMediaStream
+    if (cameraLive) {
+      captureFromWebcam({ videoSize, videoRef, canvasRef, previewRefLeft, previewRefRight, duration, setCameraLive })
+      // setIsRunning(false)
+    }
 
-        const videoTrack = localMediaStream.getVideoTracks()[0]
-        const videoTrackCapabilities = videoTrack.getCapabilities()
-        console.log({ videoTrack })
+    if ((duration / 1000) - time <= 0) {
+      setIsRunning(false)
+    }
 
-        videoRef.current.play()
+  }, [time, isRunning])
 
-        const ctx = canvasRef.current.getContext('2d')
-        const previewCtxLeft = previewRefLeft.current.getContext('2d')
-        const previewCtxRight = previewRefRight.current.getContext('2d')
-        let updateLoop = null
 
-        setTimeout(() => {
-          canvasRef.current.width = videoWidth
-          canvasRef.current.height = videoWidth
-
-          previewRefLeft.current.width = videoWidth
-          previewRefLeft.current.height = videoWidth
-
-          previewRefRight.current.width = videoWidth
-          previewRefRight.current.height = videoWidth
-
-          updateLoop = setInterval(() => {
-            ctx.drawImage(
-              videoRef.current,
-              0, 0,
-              videoRef.current.videoWidth,
-              videoRef.current.videoHeight
-            )
-            previewCtxLeft.putImageData(
-              groupPixels(ctx.getImageData(0, 0, videoWidth, videoWidth)).left,
-              0,0,0,0,
-              videoWidth,
-              videoWidth,
-            )
-            previewCtxRight.putImageData(
-              groupPixels(ctx.getImageData(0, 0, videoWidth, videoWidth)).right,
-              0,0,0,0,
-              videoWidth,
-              videoWidth,
-            )
-            // get the pixels of the image
-            // canvas width and height is available on this pixels object
-            // Useful for dynamic sizing
-            // const pixels = ctx.getImageData(0, 0, videoWidth, videoWidth)
-            // updatePixelState(pixels)
-          }, 32) // interval update
-        }, 1000)
-
-        setTimeout(() => {
-          // Cancel the update loop after timep period
-          clearInterval(updateLoop)
-          videoRef.current.pause()
-        }, 10 * 1000)
-      })
-      .catch(err => {
-        console.error('-- MEDIA STREAM ERROR --\n', err)
-      })
-  })
+  const handleButtonClick = () => {
+    if (isRunning) {
+      setIsRunning(false)
+    } else {
+      setTime(0)
+      setIsRunning(true)
+    }
+  }
 
   return <Wrapper>
     <CanvasStaging ref={ canvasRef } />
 
     <VideoWrapper>
-      <MirrorPreview>
+      <MirrorPreview showOverlay={ !isRunning }>
         <Canvas ref={ previewRefLeft } />
       </MirrorPreview>
 
       <Video ref={ videoRef } />
 
-      <MirrorPreview>
+      <MirrorPreview showOverlay={ !isRunning }>
         <Canvas ref={ previewRefRight } />
       </MirrorPreview>
     </VideoWrapper>
+
+
+    {/* <CountdownWrapper></CountdownWrapper> */}
+
+    <ControlsWrapper>
+      {
+          isRunning
+          ? <CountdownWrapper>
+              <p>Image ready in ...</p>
+              <Countdown>{ (duration / 1000) - time }</Countdown>
+            </CountdownWrapper>
+          : <Button onClick={ handleButtonClick }>
+              <PlayIcon size={ 100 } fill="#ffffff" />
+            </Button>
+      }
+    </ControlsWrapper>
     {/* <pre>
       pixels length: { pixelState?.length } | 
       grouped: { groupPixels(pixelState)?.length }
