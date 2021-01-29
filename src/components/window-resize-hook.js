e31c109bca51cb78a7eb68822f62a70b61d51d72
martin-banks <anotherbanksy@gmail.com>
2021-01-29 18:46:18 +1100

    adds hook to detect window resizing

---
+++ src/components/window-resize-hook.js
@@ -0,0 +1,27 @@
+import React, { useLayoutEffect, useState } from 'react'
+
+
+const useWindowSize = () => {
+  const [ size, setSize ] = useState([ 0, 0 ])
+  let resizeWait = null
+
+  function updateSize() {
+    if (resizeWait) {
+      clearTimeout(resizeWait)
+    }
+    resizeWait = setTimeout(() => {
+      setSize([ window.innerWidth, window.innerHeight ])
+    }, 50)
+  }
+
+  useLayoutEffect(() => {
+    window.addEventListener('resize', updateSize)
+    updateSize()
+
+    return () => window.removeEventListener('resize', updateSize)
+  }, [])
+
+  return size
+}
+
+export default useWindowSize
