34b8cdac60bfaba73269a3ce3ee5f775e38a0dec
martin-banks <anotherbanksy@gmail.com>
2021-02-04 18:53:07 +1100

    fixes errors in images not saving. -- not in use

--- src/functions/save-fullres-image.js
+++ src/functions/save-fullres-image.js
@@ -1,104 +1,77 @@
 import React, { useContext } from 'react'
 
 import groupPixels from './group-pixels'
-// import FullResContext from '../context/fullres-context'
 
 
 function storeFullResImage (props) {
   const {
-    canvasRef,
+    // canvasRef,
     setCameraLive,
     fullResRef,
     hiddenVideoRef,
     duration,
-    storeFullRes,
   } = props
 
-  const ctx = fullResRef.current.getContext('2d')
-  // const frc = FullResContext()
 
+  return new Promise((resolve, reject) => {
+    const ctx = fullResRef.current.getContext('2d')
 
-  navigator.mediaDevices
-    .getUserMedia({ audio: false, video: true })
-    .then(localMediaStream => {
-      const videoTrack = localMediaStream.getVideoTracks()[0]
-      const track = videoTrack.getCapabilities()
-      console.log({ videoTrack, track })
+    navigator.mediaDevices
+      .getUserMedia({ audio: false, video: true })
+      .then(localMediaStream => {
+        const videoTrack = localMediaStream.getVideoTracks()[0]
+        const track = videoTrack.getCapabilities()
 
-      hiddenVideoRef.current.srcObject = localMediaStream
-      hiddenVideoRef.current.play()
+        console.log({ track })
 
-      // hiddenVideoRef.current.addEventListener('playing', function () {
-      // })
+        hiddenVideoRef.current.width = `${track.width.max}px`
+        hiddenVideoRef.current.height = `${track.height.max}px`
 
-      ctx.width = track.width.max
-      ctx.height = track.height.max
+        hiddenVideoRef.current.srcObject = localMediaStream
+        hiddenVideoRef.current.play()
 
-      return new Promise((resolve, reject) => {
-        setInterval(() => {
-          ctx.drawImage(
-            hiddenVideoRef.current,
-            0, 0,
-            track.width.max,
-            track.height.max,
-          )
-        }, 42)
+        fullResRef.current.width = track.width.max
+        ctx.width = track.width.max
+        fullResRef.current.height = track.height.max
+        ctx.height = track.height.max
 
-        setTimeout(async () => {
-          hiddenVideoRef.current.pause()
-          setCameraLive(false)
-          localMediaStream.getTracks().forEach(t => t.stop())
-          const delay = 0 // 30 * 1000
-          const options = {
-            videoSize: {
-              width: track.width.max,
-              height: track.height.max,
-            },
-            pixels: ctx.getImageData(0, 0, track.width.max, track.height.max),
-            delay,
-          }
-          console.log({ options })
-          const groupedPixels_fullSize = await groupPixels(options)
-          resolve(groupedPixels_fullSize)
-        }, duration)
+        return new Promise((resolve, reject) => {
+          setInterval(() => {
+            ctx.drawImage(
+              hiddenVideoRef.current,
+              0, 0,
+              track.width.max,
+              track.height.max,
+            )
+          }, 42)
 
+          setTimeout(async () => {
+            hiddenVideoRef.current.pause()
+            setCameraLive(false)
+            localMediaStream.getTracks().forEach(t => t.stop())
+            const delay = 0 // 30 * 1000
+            const options = {
+              videoSize: {
+                width: track.width.max,
+                height: track.height.max,
+              },
+              pixels: ctx.getImageData(0, 0, track.width.max, track.height.max),
+              delay,
+            }
+            const groupedPixels_fullSize = await groupPixels(options)
+            resolve(groupedPixels_fullSize)
+          }, duration + 100)
+
+        })
       })
-
-      // setTimeout(async () => {
-      //   // videoRef.current.pause()
-      //   // setCameraLive(false)
-      //   // localMediaStream.getTracks().forEach(t => t.stop())
-      //   const delay = 0 // 30 * 1000
-      //   const options = {
-      //     videoSize: {
-      //       width: track.width.max,
-      //       height: track.height.max,
-      //     },
-      //     pixels: ctx.getImageData(0, 0, track.width.max, track.height.max),
-      //     delay,
-      //   }
-      //   console.log({ options })
-      //   const groupedPixels_fullSize = await groupPixels(options)
-
-      // }, 100)
-
-      // return true // groupedPixels
-    })
-    .then(data => {
-      console.log({ data })
-      // const { setData } = useContext(FullResContext)
-      // setData(data)
-      // FullResContext.setData(data)
-      console.log({ storeFullRes })
-      storeFullRes(data)
-    })
-    .catch(error => {
-      console.error('-- ERROR CREATING FULL IMAGE --\n', error)
-      console.trace(error.trace)
-      throw error
-    })
+      .then(data => {
+        resolve(data)
+      })
+      .catch(error => {
+        throw reject(error)
+      })
+  })
 }
-
 
 
 export default storeFullResImage
