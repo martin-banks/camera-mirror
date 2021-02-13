63c1f2f57c9f63331953d9d382c5111246ef089c
martin-banks <anotherbanksy@gmail.com>
2021-02-13 16:00:33 +1100

    removes old icons files

--- src/components/icons/index.js
+++
@@ -1,9 +0,0 @@
-import email from './icon-email@3x.png'
-import github from './icon-github-mark@3x.png'
-import linkedin from './icon-linkedin-mark@3x.png'
-
-export default {
-  email,
-  github,
-  linkedin,
-}
